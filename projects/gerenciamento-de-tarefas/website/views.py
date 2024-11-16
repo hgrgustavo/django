72c144d66435d1981f64337674f35bfd76589636
gus <gustavohgrdev@gmaiil.com>
2024-11-16 12:54:01 -0300

    building view layer

--- projects/gerenciamento-de-tarefas/website/views.py
+++ projects/gerenciamento-de-tarefas/website/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from website.models import Usuario, Tarefa
+
+
+class CadastroUsuario(CreateView):
+    template_name = "cadastro_usuario.html"
+    model = Usuario
+    fields = ["usu_nome", "usu_email"]
+    success_url = "tarefas.html"
+
+
+class CadastroTarefas(CreateView):
+    template_name = "cadastro_tarefas.html"
+    model = Tarefa
+    fields = ["tar_descricao", "tar_setor", "usu_codigo", "tar_prioridade"]
+    success_url = "tarefas.html"
+
+
+class ListaTarefas(ListView):
+    model = Tarefa
+    paginate_by = 3
