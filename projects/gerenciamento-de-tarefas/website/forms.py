833315cdc37135bcd332750cac9a72e044ff39dd
gus <gustavohgrdev@gmaiil.com>
2024-11-23 21:07:22 -0300

    working with forms

--- projects/gerenciamento-de-tarefas/website/forms.py
+++ projects/gerenciamento-de-tarefas/website/forms.py
@@ -1,33 +1,14 @@
-from django.forms import ModelForm, modelformset_factory, TextInput, Select
-
-from . import models
-
-
-class CadastroUsuarioForm(ModelForm):
-    class Meta:
-        model = models.Usuario
-        fields = ["usu_nome", "usu_email"]
-
-
-CadastroUsuarioFormSet = modelformset_factory(models.Usuario, form=CadastroUsuarioForm,
-    fields=["usu_nome", "usu_email"],
-    widgets={"usu_nome": TextInput(attrs={"placeholder": "Nome", "required": "required"}),
-        "usu_email": TextInput(attrs={"placeholder": "Email", "required": "required"})
-    }
-)
+from django.forms import modelformset_factory, ModelForm
+from website.models import Tarefa, Usuario
 
 
 class CadastroTarefaForm(ModelForm):
     class Meta:
-        model = models.Tarefa
-        fields = ["tar_descricao", "tar_setor", "usu_codigo", "tar_prioridade"]
+        model = Tarefa
+        fields = [
+            "tar_descricao",
+            "tar_setor",
+            "usu_codigo",
+            "tar_prioridade"
+        ]
 
-
-CadastroTarefaFormSet = modelformset_factory(models.Tarefa, CadastroTarefaForm,
-    fields=["tar_descricao", "tar_setor", "usu_codigo", "tar_prioridade"],
-    widgets={"tar_descricao": TextInput(attrs={"placeholder": "Descrição", "required": "required"}),
-        "tar_setor": TextInput(attrs={"placeholder": "Setor", "required": "required"}),
-        "usu_codigo": Select(attrs={"required": "required"}),
-        "tar_prioridade": Select(attrs={"required": "required"})
-    }
-)
--- projects/gerenciamento-de-tarefas/website/views.py
+++ projects/gerenciamento-de-tarefas/website/views.py
@@ -1,38 +1,12 @@
-from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
-from django.views.generic.list import ListView
-
-from website.forms import CadastroUsuarioForm, CadastroUsuarioFormSet
+from django.views.generic import TemplateView, FormView
 from website.models import Usuario, Tarefa
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from website.forms import CadastroTarefaForm
 
 
-class CadastroUsuario(CreateView):
-    template_name = "cadastro_usuario.html"
-    model = Usuario
-    fields = ["usu_nome", "usu_email"]
-    success_url = "minhas_tarefas.html"
-
-    def generate_form(self, request):
-        return render(request, "cadastro_usuario.html", {"formset": CadastroUsuarioFormSet})
+class CadastroTarefas(FormView):
+    context_object_name = "tarefa_object_list"
 
 
-
-
-
-class CadastroTarefas(CreateView):
-    template_name = "cadastro_tarefas.html"
-    model = Tarefa
-    fields = ["tar_descricao", "tar_setor", "usu_codigo", "tar_prioridade"]
-    success_url = "minhas_tarefas.html"
-
-
-
-class MinhasTarefas(ListView):
-    template_name = "minhas_tarefas.html"
-    model = Tarefa
-    paginate_by = 3
 
 
 class EmptyPathView(TemplateView):
